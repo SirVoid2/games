64b156e27e42b9f49debfa052d2b2615381751b8
SirVoid2 <techandbiketips@gmail.com>
2025-10-28 13:58:23 -0500

    d

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// List of game paths
-const games = [
-  "games/1v1LoL",
-  "games/Ant Art Tycoon",
-  "games/BitLife",
-  "games/Bloxorz",
-  "games/Blackjack",
-  "games/Cookie Clicker",
-  "games/Crossy Road",
-  "games/Drift Boss",
-  "games/Drive Mad",
-  "games/Fruit Ninja",
-  "games/Minecraft",
-  "games/Retro Bowl",
-  "games/Retro Bowl College"
+// List of ame paths
+const ǧames = [
+  "ǧames/1v1LoL",
+  "ǧames/Ant Art Tycoon",
+  "ǧames/BitLife",
+  "ǧames/Bloxorz",
+  "ǧames/Blackjack",
+  "ǧames/Cookie Clicker",
+  "ǧames/Crossy Road",
+  "ǧames/Drift Boss",
+  "ǧames/Drive Mad",
+  "ǧames/Fruit Ninja",
+  "ǧames/Minecraft",
+  "ǧames/Retro Bowl",
+  "ǧames/Retro Bowl College"
 ];
 
 const container = document.querySelector('div');
 
-games.forEach(game => {
+ǧames.forEach(ǧame => {
   const link = document.createElement('a');
-  link.href = game; // Set the href to the game path
-  link.textContent = game.split('/')[1]; // Use the game name after the slash
-  link.className = 'game-link';
+  link.href = ǧame; // Set the href to the ǧame path
+  link.textContent = ǧame.split('/')[1]; // Use the ǧame name after the slash
+  link.className = 'ǧame-link';
 
   // Optional: Open links in a new tab
   link.target = '_blank';
